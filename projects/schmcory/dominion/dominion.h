2eada9a9d0a4474a99af9b514f74ca9944e40b0d
schmcory <36436049+schmcory@users.noreply.github.com>
2019-07-07 11:31:59 -0400

    Added function prototypes

--- projects/schmcory/dominion/dominion.h
+++ projects/schmcory/dominion/dominion.h
@@ -85,7 +85,7 @@ int initializeGame(int numPlayers, int kingdomCards[10], int randomSeed,
 		   struct gameState *state);
 /* Responsible for initializing all supplies, and shuffling deck and
    drawing starting hands for all players.  Check that 10 cards selected
-   are in fact (different) kingdom cards, and that numPlayers is valid. 
+   are in fact (different) kingdom cards, and that numPlayers is valid.
 
 Cards not in game should initialize supply position to -1 */
 
@@ -127,5 +127,12 @@ int scoreFor(int player, struct gameState *state);
 int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
+
+int mineRefactor(struct gameState *state, int choice1, int choice2, int handPos);
+int ambassadorRefactor(struct gameState *state, int choice1, int choice2, int handPos);
+int minionRefactor(struct gameState *state, int handPos, int choice1, int choice2);
+int baronRefactor(struct gameState *state, int choice1);
+int tributeRefactor(struct gameState *state);
+
 
 #endif
