78cc197771474d8ebf3b887afd2807d4c5f6f961
hahmed01 <40370629+hahmed01@users.noreply.github.com>
2019-07-08 02:38:21 -0400

    Fixed error

--- projects/ahmedhay/dominion/dominion_helpers.h
+++ projects/ahmedhay/dominion/dominion_helpers.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _DOMINION_HELPERS_H
 #define _DOMINION_HELPERS_H
 
@@ -21,20 +20,3 @@ int mineEffect(struct gameState *state, int choice1, int choice2, int handPos, i
 
 
 #endif
-=======
-#ifndef _DOMINION_HELPERS_H
-#define _DOMINION_HELPERS_H
-
-#include "dominion.h"
-
-int drawCard(int player, struct gameState *state);
-int updateCoins(int player, struct gameState *state, int bonus);
-int discardCard(int handPos, int currentPlayer, struct gameState *state, 
-		int trashFlag);
-int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
-int getCost(int cardNumber);
-int cardEffect(int card, int choice1, int choice2, int choice3, 
-	       struct gameState *state, int handPos, int *bonus);
-
-#endif
->>>>>>> 9c0f7f34120413bbf5b7ec5875dd405879540264
