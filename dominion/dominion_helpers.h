44d0170e50103dbf6ca74b633125e539c5041e2c
Jessica <atenj@oregonstate.edu>
2019-07-08 01:58:55 -0400

    Refactored cardEffect, added 5 card functions.  Introduced 20 bugs.

--- dominion/dominion_helpers.h
+++ dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int cardAmbassador(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardBaron(int choice1, struct gameState *state, int currentPlayer);
+int cardMine(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardMinion(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardTribute(struct gameState *state, int nextPlayer, int currentPlayer, int *tributeRevealedCards);
 
 #endif
