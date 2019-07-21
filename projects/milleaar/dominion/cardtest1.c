7a449c8f1846f97c6056fa49cdb2e7ac8d07d384
Aaron Miller <milleaar@oregonstate.edu>
2019-07-21 15:12:12 -0500

    Added last of the unit tests and modified makefile to output results

--- projects/milleaar/dominion/cardtest1.c
+++ projects/milleaar/dominion/cardtest1.c
@@ -17,267 +17,238 @@
 // set NOISY_TEST to 0 to remove printfs from output
 #define NOISY_TEST 1
 
+void checkSupply(struct gameState* state, int k[10], int players, float* testsPassed, float* totalTests){
+    int pass = 1;
+    int curseCount = 0, victoryCount = 0, hallGarden = 0;
+    int kingdom = 10;
+    int treasure = 60 - (7 * players);
+    switch(players) {
+        case 2:
+            curseCount = 10;
+            victoryCount = 8;
+            hallGarden = 8;
+            break;
+        case 3:
+            curseCount = 20;
+            victoryCount = 12;
+            hallGarden = 12;
+            break;
+        case 4:
+            curseCount = 30;
+            victoryCount = 12;
+            hallGarden = 12;
+            break;
+    }
+
+    //testing supply of curse
+    if (state->supplyCount[curse] != curseCount){
+        pass = 0;
+#if (NOISY_TEST == 1)
+        printf("\tCurse amount is incorrect\n");
+#endif 
+    }
+    //testing victory counts
+    if (state->supplyCount[estate] != victoryCount
+        || state->supplyCount[duchy] != victoryCount
+        || state->supplyCount[province] != victoryCount){
+            pass = 0;
+#if (NOISY_TEST == 1)
+             printf("\tVictory amount is incorrect\n");
+#endif 
+    }
+    //testing treasure amount
+    if (state->supplyCount[copper] != treasure) {
+        pass = 0;
+#if (NOISY_TEST == 1)
+        printf("\tTreasure amount is incorrect\n");
+#endif 
+    }
+    //testing the kingdom cards were initialized    
+
+    for (int j = 0; j < 10; j++)
+    {
+        if(k[j] != great_hall && k[j] != gardens)
+        {
+            if(state->supplyCount[ k[j] ] != kingdom)
+            {
+                pass = 0;
+#if (NOISY_TEST == 1)
+                printf("\tKingdom amount is incorrect\n");
+#endif 
+            }
+        }
+    }
+    //testing great halls and gardens if they are present in the deck
+    //also testing their supply counts
+    if (state->supplyCount[great_hall] != hallGarden) {
+        pass = 0;
+#if (NOISY_TEST == 1)
+        printf("\tGreat Hall amount is incorrect\n");
+#endif 
+    }
+    if (state->supplyCount[gardens] != hallGarden) {
+        pass = 0;
+#if (NOISY_TEST == 1)
+        printf("\tGardens amount is incorrect\n");
+#endif 
+    }
+
+    if (pass) 
+    {
+        (*testsPassed)++;
+        (*totalTests)++;
+#if (NOISY_TEST == 1)
+        printf("\tPASS. Supply counts were all correct\n");
+#endif
+    }
+    else 
+    {
+        (*totalTests)++;
+#if (NOISY_TEST == 1)
+        printf("\tFAILED. Supply counts were incorrect\n");
+#endif
+    }
+
+}
+
+void checkDecks(struct gameState* state, float* testsPassed, float* totalTests){
+    //must check that the player decks are set to correct amounts
+    int players = state->numPlayers;
+    int currentPlayer = whoseTurn(state);
+    int pass = 1;
+    for(int i = 0; i < players; i++) {
+        if(i == currentPlayer) {
+            if(state->deckCount[i] != 5 || state->handCount[i] != 5) {
+                pass = 0;
+#if (NOISY_TEST == 1)
+                printf("\tCurrent player card counts are incorrect\n");
+#endif
+            }
+        }
+        else if(state->deckCount[i] != 10 || state->handCount[i] != 0){
+                pass = 0;
+#if (NOISY_TEST == 1)
+                printf("\tOther player card counts are incorrect\n");
+#endif   
+        }
+    }
+
+    if (pass) 
+    {
+        (*testsPassed)++;
+        (*totalTests)++;
+#if (NOISY_TEST == 1)
+        printf("\tPASS. Deck counts were all correct\n");
+#endif
+    }
+    else 
+    {
+        (*totalTests)++;
+#if (NOISY_TEST == 1)
+        printf("\tFAILED. Deck counts were incorrect\n");
+#endif
+    }
+
+}
+
 int main()
 {
     int seed = 1000;
     int numPlayer = 2;
-    int choice1, choice2, r;
-    int k[10] = {adventurer, council_room, feast, gardens, mine
+    int r;
+    int k[10] = {adventurer, council_room, feast, baron, mine
+                 ,
+                 remodel, smithy, village, gardens, great_hall};
+    int badCards[10] = {adventurer, adventurer, feast, gardens, mine
                  ,
                  remodel, smithy, village, baron, great_hall};
-    struct gameState beforeState;
 
     float testsPassed = 0;
     float totalTests = 0;
 
+
 #if (NOISY_TEST == 1)
-    printf("TESTING mineEffect:\n");
+    printf("TESTING initializeGame:\n");
 #endif
 
+	// - players MAX + 1 and 1, expect -1
+#if (NOISY_TEST == 1)
+        printf("\tTesting Max + 1 player count\n");
+#endif
+    struct gameState G;
+    r = initializeGame(MAX_PLAYERS + 1, k, seed, &G);
+    if (r == -1) 
+    {
+        testsPassed++;
+        totalTests++;
+#if (NOISY_TEST == 1)
+        printf("\tPASS. Caught invalid player count parameter\n");
+#endif
+    }
+    else 
+    {
+        totalTests++;
+#if (NOISY_TEST == 1)
+        printf("\tFAILED. Did not catch invalid player count parameter\n");
+#endif
+    }
+#if (NOISY_TEST == 1)
+        printf("\tTesting 1 player count\n");
+#endif
+    r = initializeGame(1, k, seed, &G);
+    if (r == -1) 
+    {
+        testsPassed++;
+        totalTests++;
+#if (NOISY_TEST == 1)
+        printf("\tPASS. Caught invalid player count parameter\n");
+#endif
+    }
+    else 
+    {
+        totalTests++;
+#if (NOISY_TEST == 1)
+        printf("\tFAILED. Did not catch invalid player count parameter\n");
+#endif
+    }
+
+	// - set duplicate kingdom cards, expect -1
+#if (NOISY_TEST == 1)
+        printf("\tTesting Duplicate player cards\n");
+#endif
+    r = initializeGame(numPlayer, badCards, seed, &G);
+    if (r == -1) 
+    {
+        testsPassed++;
+        totalTests++;
+#if (NOISY_TEST == 1)
+        printf("\tPASS. Caught duplicate player cards\n");
+#endif
+    }
+    else 
+    {
+        totalTests++;
+#if (NOISY_TEST == 1)
+        printf("\tFAILED. Did not catch duplicate player cards\n");
+#endif
+    }
+
+	// - test curse card amounts 2,3,4 and victory card amounts 2,3
+	// - check supply of great hall or gardens after setup of deck
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 4; j++)
-        {
-            switch (i)
-            {
-                case 0:
-                    choice1 = estate;
-                    break;
-
-                case 1:
-                    choice1 = copper;
-                    break;
-
-                case 2:
-                    choice1 = gold;
-            }
-
-            switch (j)
-            {
-                case 0:
-                    choice2 = -1;
-                    break;
-
-                case 1:
-                    choice2 = copper;
-                    break;
-
-                case 2:
-                    choice2 = silver;
-                    break;
-
-                case 3:
-                    choice2 = gold;
-            }
-
+        int totalPlayers = numPlayer + i;
 #if (NOISY_TEST == 1)
-            printf("Testing with choice1 = %d, choice2 = %d\n", choice1, choice2);
+        printf("\tTesting supplies with %d players\n", totalPlayers);
 #endif
+        r = initializeGame(totalPlayers, k, seed, &G);
+        checkSupply(&G, k, totalPlayers, &testsPassed, &totalTests);
+        checkDecks(&G, &testsPassed, &totalTests);
 
-            //make new game
-            struct gameState G;
-            r = initializeGame(numPlayer, k, seed, &G);
-            int currentPlayer = whoseTurn(&G);
-
-            G.hand[currentPlayer][0] = mine;
-            G.hand[currentPlayer][1] = choice1;
-
-            //printing hand contents
-            for(int handPos = 0; handPos < G.handCount[currentPlayer]; handPos++){
-             printf("Card %d : %d\n", handPos, G.hand[currentPlayer][handPos]);
-             }
-
-            beforeState = G;
-
-            int result = mineEffect( 1, choice2, currentPlayer, &G, 0);
-for(int handPos = 0; handPos < G.handCount[currentPlayer]; handPos++){
-             printf("Card %d : %d\n", handPos, G.hand[currentPlayer][handPos]);
-             }
-            //check treasure card amounts
-            int beforeCopper = 0, beforeSilver = 0, beforeGold = 0;
-            int afterCopper = 0, afterSilver = 0, afterGold = 0;
-
-            for (int p = 0; p < beforeState.handCount[currentPlayer]; p++)
-            {
-                if (beforeState.hand[currentPlayer][p] == copper)
-                {
-                    beforeCopper++;
-                }
-                else if (beforeState.hand[currentPlayer][p] == silver)
-                {
-                    beforeSilver++;
-                }
-                else if (beforeState.hand[currentPlayer][p] == gold)
-                {
-                    beforeGold++;
-                }
-            }
-
-            for (int p = 0; p < G.handCount[currentPlayer]; p++)
-            {
-                if (G.hand[currentPlayer][p] == copper)
-                {
-                    afterCopper++;
-                }
-                else if (G.hand[currentPlayer][p] == silver)
-                {
-                    afterSilver++;
-                }
-                else if (G.hand[currentPlayer][p] == gold)
-                {
-                    afterGold++;
-                }
-            }
-            //giving bad choices
-            if (i == 0 || j == 0)
-            {
-                if (result == -1)
-                {
-                    testsPassed++;
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tPASS. Caught bad input\n");
-#endif
-                }
-                else
-                {
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tFAILED. Missed bad input\n");
-#endif
-                }
-            }
-            //if choice1 copper and choice2 is copper or silver
-            else if (i == 1 && (j == 1 || j == 2))
-            {
-                if (result == -1)
-                {
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tFAILED. Was valid combination of treasures\n");
-#endif
-                }
-                else if (j == 1) 
-                {
-                    if (beforeCopper == afterCopper) {
-                        testsPassed++;
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tPASS. Traded Copper for Copper\n");
-#endif
-                    }
-                    else {
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tFAILED. Did not trade cards correctly\n");
-#endif
-                    }
-                }
-                else if (j == 2) {
-                    if (beforeCopper - 1 == afterCopper && beforeSilver == afterSilver - 1) {
-                        testsPassed++;
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tPASS. Traded Copper for Silver\n");
-#endif
-                    }
-                    else {
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tFAILED. Did not trade cards correctly\n");
-#endif
-                    }
-                }
-
-            }
-            //if choice1 is copper and choice2 is gold
-            else if (i == 1 && j == 3)
-            {
-                if (result == -1)
-                {
-                    testsPassed++;
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tPASS. Caught bad combination\n");
-#endif
-                }
-            }
-            //if choice1 is gold and choice 2 is copper or silver
-            else if (i == 2 && (j == 1 || j == 2))
-            {
-                if (result == -1)
-                {
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tFAILED. Was valid combination of treasures\n");
-#endif
-                }
-                else if (j == 1) 
-                {
-                    if (beforeCopper + 1 == afterCopper && beforeGold == afterGold + 1) {
-                        testsPassed++;
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tPASS. Traded Gold for Copper\n");
-#endif
-                    }
-                    else {
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tFAILED. Did not trade cards correctly\n");
-#endif
-                    }
-                }
-                else if (j == 2) {
-                    if (beforeGold - 1 == afterGold && beforeSilver == afterSilver - 1) {
-                        testsPassed++;
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tPASS. Traded Gold for Silver\n");
-#endif
-                    }
-                    else {
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tFAILED. Did not trade cards correctly\n");
-#endif
-                    }
-                }
-            }
-
-            //if choice1 is gold and choice2 is gold
-            else if (i == 2 && j == 3)
-            {
-                if (result == -1)
-                {
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tFAILED. Was valid combination of treasures\n");
-#endif
-                }
-                else if (beforeGold == afterGold) {
-                        testsPassed++;
-                        totalTests++;
-#if (NOISY_TEST == 1)
-                        printf("\tPASS. Traded Gold for Gold\n");
-#endif
-                }
-                else {
-                    totalTests++;
-#if (NOISY_TEST == 1)
-                    printf("\tFAILED. Did not trade cards correctly\n");
-#endif
-                }
-
-
-            }
-
-#if (NOISY_TEST == 1)
-            printf("\tBefore Copper: %d\tAfter Copper: %d\n", beforeCopper, afterCopper);
-            printf("\tBefore Silver: %d\tAfter Silver: %d\n", beforeSilver, afterSilver);
-            printf("\tBefore Gold: %d\t\tAfter Gold: %d\n", beforeGold, afterGold);
-#endif
-        }
+        // for (i = adventurer; i <= treasure_map; i++) //loop all cards
+        // {
+        //     printf("Card%d: %d\n", i, G.supplyCount[i]);
+        // }
     }
 
     float percentPass = (testsPassed / totalTests) * 100.0;
