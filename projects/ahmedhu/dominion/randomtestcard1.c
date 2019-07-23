05665332b96225344a939b81df7b5c489b56b45e
Humza Ahmed <hsa5037@gmail.com>
2019-07-23 15:07:11 -0400

    Assignment 4

---
+++ projects/ahmedhu/dominion/randomtestcard1.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+int main (){
+	int *k = kingdomCards(ambassador, village, minion, gardens, smithy, tribute, baron, cutpurse, mine, outpost);
+	int passed = 0;
+	int failed = 0;
+	int tests = 50;
+
+	int i;
+
+	for (int i = 0; i < tests; i++){
+		//randomize player count, seed random
+		int seed = rand();
+		int players = rand() % 4;
+		int currPlayer = 0;
+		int choice1 = rand() % 2;
+
+		struct gameState *game = malloc(sizeof(struct gameState));
+		initializeGame(players, k, seed, game);
+
+		//randomize discard/deck/handcount
+		game->handCount[currPlayer] = rand() % MAX_HAND;
+		game->deckCount[currPlayer] = rand() % MAX_DECK;
+		game->discardCount[currPlayer] = rand() % MAX_DECK;		
+
+
+		//Save stats before effect
+		int preBuys = game->numBuys;
+		int preCoins = game->coins;
+		int preEstateCount;
+		int preHandCount = game->handCount[currPlayer];
+		for (int i = 0; i <= preHandCount; i++){
+			if (game.hand[currPlayer][i] == estate){
+				preEstateCount++
+			}
+		}
+
+		cardEffect(baron, choice1, 1, 1, &game, 0, 0);
+
+		int postEstateCount;
+
+		if (preEstateCount > 0 && choice1 == 1 && preHandCount - 2 == game->handCount[currPlayer] &&  preBuys + 1 == game->numBuys && preCoins + 4 = game->coins){
+			for (int i = 0; i <= game->handCount; i++){
+				if (game.hand[currPlayer][i] == estate){
+					postEstateCount++;
+				}
+			}
+			if (postEstateCount == preEstateCount - 1){
+				passed++;
+			} else{
+				failed++;
+			}
+		}
+		else if (choice1 == 0 && preBuys + 1 == game->numBuys){
+			for (int i = 0; i <= game->handCount; i++){
+				if (game.hand[currPlayer][i] == estate){
+					postEstateCount++;
+				}
+			}
+			if (postEstateCount == preEstateCount + 1){
+				passed++;
+			} else{
+				failed++;
+			}
+		}
+
+		free(game);
+	}
+	printf("Random Test of Baron Card!\n");
+	printf("There were %d successful tests and %d failed tests out of %d games!\n", passed, failed, tests);
+
+	return 0;
+
+}
